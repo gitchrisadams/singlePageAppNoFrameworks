524ae298f908098abc7ad978cc58fdbb7bbcf082
Christopher Adams <chrismichaeladams@gmail.com>
2017-06-25 11:39:47 -0400

    Grunt, watch, es6 transpile all working

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,28 @@
-import Timer from "./module.js";
+"use strict";
 
-let timeElement = new Timer( document.querySelector("time") );
+// Import Underscore library:
+var _ = require('underscore'),
+  names = ['Bruce Wayne', 'Wally West', 'John Jones', 'Kyle Rayner', 'Arthur Curry', 'Clark Kent'],
+  otherNames = ['Barry Allen', 'Hal Jordan', 'Kara Kent', 'Diana Prince', 'Ray Palmer', 'Oliver Queen'];
 
-timeElement.update();
+// Import user defined module:
+var module1 = require('./module1.js');
 
-console.log("Hello from index.js after timeElement");
+let myLet1 = "test";
 
+// Use imported module
+module1();
+
+_.each([names, otherNames], function(nameGroup) {
+  findSuperman(nameGroup);
+});
+
+function findSuperman(values) {
+  _.find(values, function(name) {
+    if (name === 'Clark Kent') {
+      console.log('It\'s Superman!');
+    } else {
+      console.log('... No superman!');
+    }
+  });
+}
---
+++ src/scripts/module1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Export module:
+module.exports = function() {
+
+	console.log("From module1, Browserified!");
+}
